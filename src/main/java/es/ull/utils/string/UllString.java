8843988b5a491baebaab01a37843f0c8c0adf257
cexposit <cexposit@ull.edu.es>
2022-11-07 17:09:26 +0000

    mc

--- src/main/java/es/ull/utils/string/UllString.java
+++ src/main/java/es/ull/utils/string/UllString.java
@@ -1,14 +1,5 @@
 package es.ull.utils.string;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Set;
-import java.util.regex.Pattern;
-import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.commons.validator.routines.EmailValidator;
-
 public class UllString {
 
 }
