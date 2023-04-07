c2d62b334977e164dc5b23de6247213672108066
cexposit <cexposit@ull.edu.es>
2023-04-07 14:36:13 +0100

    mc

--- src/main/java/es/ull/utils/string/UllString.java
+++ src/main/java/es/ull/utils/string/UllString.java
@@ -1,5 +1,9 @@
 package es.ull.utils.string;
 
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class UllString {
@@ -25,5 +29,22 @@ public class UllString {
                 .replaceAll("\t", " ")
                 .replaceAll(" +", " ")
                 .trim();
+    }
+
+    public static String random(int length, Pattern pattern) {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        while (sb.length() < length) {
+            Matcher matcher = pattern.matcher(Character.toString((char) random.nextInt(128)));
+            if (matcher.matches()) {
+                sb.append(matcher.group());
+            }
+        }
+        return sb.toString();
+    }
+
+    public static String random(int length, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        return UllString.random(length, pattern);
     }
 }
