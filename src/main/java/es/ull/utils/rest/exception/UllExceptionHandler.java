7cd4f8831838df1e59c84999b9463d13cf1fed9a
cexposit <cexposit@ull.edu.es>
2024-01-21 18:54:22 +0000

    new version of dependencies

--- src/main/java/es/ull/utils/rest/exception/UllExceptionHandler.java
+++ src/main/java/es/ull/utils/rest/exception/UllExceptionHandler.java
@@ -1,12 +1,16 @@
 package es.ull.utils.rest.exception;
 
+import es.ull.utils.date.UllDate;
+import es.ull.utils.rest.error.ApiError;
+import es.ull.utils.rest.error.ApiSubError;
+
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -18,9 +22,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import es.ull.utils.date.UllDate;
-import es.ull.utils.rest.error.ApiError;
-import es.ull.utils.rest.error.ApiSubError;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -29,47 +30,58 @@ public class UllExceptionHandler extends ResponseEntityExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(UllExceptionHandler.class);
 
-    public static ApiError createBody(int status, String path, String message) {
+    public static ApiError createBody(
+            HttpStatus status,
+            String path,
+            String message) {
         ApiError body = new ApiError();
         body.setStatus(status);
-        if (status == 400) {
-            body.setError(ApiError.MESSAGE_BAD_REQUEST);
-        } else if (status == 404) {
-            body.setError(ApiError.MESSAGE_NOT_FOUND);
-        }
         body.setPath(path);
         body.setTimestamp(UllDate.now());
         body.setMessage(message);
         return body;
     }
 
-    public static ApiError createBody(int status, String path, String message, List<ApiSubError> details) {
+    public static ApiError createBody(
+            HttpStatus status,
+            String path,
+            String message,
+            List<ApiSubError> details) {
         ApiError body = UllExceptionHandler.createBody(status, path, message);
         body.setDetails(details);
         return body;
     }
 
-    public static ApiError createBody(UllException exception, WebRequest request) {
-        return UllExceptionHandler.createBody(exception.getStatus(),
+    public static ApiError createBody(
+            UllException exception,
+            WebRequest request) {
+        return UllExceptionHandler.createBody(
+                HttpStatus.resolve(exception.getStatus()),
                 ((ServletWebRequest) request).getRequest().getRequestURI(), exception.getMessage(),
                 exception.getSubErrors());
     }
 
     @ExceptionHandler(UllException.class)
-    protected ResponseEntity<ApiError> handleUllException(UllException exception, WebRequest request) {
+    protected ResponseEntity<ApiError> handleKaiztenException(
+            UllException exception,
+            WebRequest request) {
         logger.info("UllException");
-        return ResponseEntity.badRequest().body(UllExceptionHandler.createBody(exception, request));
+        return ResponseEntity.badRequest()
+                .body(UllExceptionHandler.createBody(exception, request));
     }
 
     @ExceptionHandler(UllBadRequestException.class)
-    protected ResponseEntity<ApiError> handleUllBadRequestException(UllBadRequestException exception,
+    protected ResponseEntity<ApiError> handleKaiztenBadRequestException(
+            UllBadRequestException exception,
             WebRequest request) {
         logger.info("UllBadRequestException");
-        return ResponseEntity.badRequest().body(UllExceptionHandler.createBody(exception, request));
+        return ResponseEntity.badRequest()
+                .body(UllExceptionHandler.createBody(exception, request));
     }
 
     @ExceptionHandler(UllNotFoundException.class)
-    protected ResponseEntity<ApiError> handleUllNotFoundException(UllNotFoundException exception,
+    protected ResponseEntity<ApiError> handleKaiztenNotFoundException(
+            UllNotFoundException exception,
             WebRequest request) {
         logger.info("UllNotFoundException");
         return ResponseEntity.status(exception.getStatus())
@@ -78,54 +90,76 @@ public class UllExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
-            HttpRequestMethodNotSupportedException exception, HttpHeaders headers, HttpStatus status, WebRequest request) {
+            HttpRequestMethodNotSupportedException exception,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
         logger.info("HttpRequestMethodNotSupportedException");
-        String[] supportedMethods = exception.getSupportedMethods();
+        final String[] supportedMethods = exception.getSupportedMethods();
         if (supportedMethods != null) {
-            headers.add("Allow", StringUtils.arrayToDelimitedString(supportedMethods, ", "));
+            headers.add(HttpHeaders.ALLOW, StringUtils.arrayToDelimitedString(supportedMethods, ", "));
         }
         headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        ApiError body = UllExceptionHandler.createBody(status.value(),
-                ((ServletWebRequest) request).getRequest().getRequestURI(), exception.getMessage());
-        return super.handleExceptionInternal(exception, body, headers, status, request);
+        final String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+        final String message = exception.getMessage();
+        return ResponseEntity.status(status)
+                .body(UllExceptionHandler.createBody((HttpStatus) status, path, message));
     }
 
     @Override
-    protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException exception,
-            HttpHeaders headers, HttpStatus status, WebRequest request) {
+    protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(
+            HttpMediaTypeNotAcceptableException exception,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
         logger.info("HttpMediaTypeNotAcceptableException");
         headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        ApiError body = UllExceptionHandler.createBody(status.value(),
-                ((ServletWebRequest) request).getRequest().getRequestURI(), exception.getMessage());
+        ApiError body = UllExceptionHandler.createBody(
+                (HttpStatus) status,
+                ((ServletWebRequest) request).getRequest().getRequestURI(),
+                exception.getMessage());
         return super.handleExceptionInternal(exception, body, headers, status, request);
     }
 
     @Override
-    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException exception,
-            HttpHeaders headers, HttpStatus status, WebRequest request) {
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
+            HttpMediaTypeNotSupportedException exception,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
         logger.info("HttpMediaTypeNotSupportedException");
         headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        ApiError body = UllExceptionHandler.createBody(status.value(),
+        ApiError body = UllExceptionHandler.createBody(
+                (HttpStatus) status,
                 ((ServletWebRequest) request).getRequest().getRequestURI(), ApiError.MESSAGE_UNSUPPORTED_MEDIA_TYPE);
         return super.handleExceptionInternal(exception, body, headers, status, request);
     }
 
     @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException exception,
-            HttpHeaders headers, HttpStatus status, WebRequest request) {
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException exception,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
         logger.info("HttpMessageNotReadableException");
         headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        ApiError body = UllExceptionHandler.createBody(status.value(),
-                ((ServletWebRequest) request).getRequest().getRequestURI(), ApiError.MESSAGE_NOT_READABLE);
+        ApiError body = UllExceptionHandler.createBody(
+                (HttpStatus) status,
+                ((ServletWebRequest) request).getRequest().getRequestURI(),
+                ApiError.MESSAGE_NOT_READABLE);
         return super.handleExceptionInternal(exception, body, headers, status, request);
     }
 
     @Override
-    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException exception,
-            HttpHeaders headers, HttpStatus status, WebRequest request) {
+    protected ResponseEntity<Object> handleNoHandlerFoundException(
+            NoHandlerFoundException exception,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
         logger.info("NoHandlerFoundException");
         headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        ApiError body = UllExceptionHandler.createBody(status.value(),
+        ApiError body = UllExceptionHandler.createBody(
+                (HttpStatus) status,
                 ((ServletWebRequest) request).getRequest().getRequestURI(), ApiError.MESSAGE_ENDPOINT_NOT_VALID);
         logger.info(headers.toString());
         logger.info(body.toString());
